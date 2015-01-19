7fec7214c2b3c1fe9651a62838e2297984025564
Rylan Bowers <rylanb@gmail.com>
2015-01-18 19:49:14 -0700

    Moving data fetching into the model. Adding helper functions to parse returned data. Bookmarks and Categories.

--- assets/js/common/models/bookmarks-model.js
+++ assets/js/common/models/bookmarks-model.js
@@ -1,1 +1,23 @@
-angular.module('eggly.models.bookmarks', []);
+angular.module('eggly.models.bookmarks', [
+])
+  .service('BookmarksModel', function($http, $q) {
+    var model = this,
+      URLS = {
+        FETCH: 'data/bookmarks.json'
+      },
+      bookmarks;
+
+    function extract(result) {
+      return result.data;
+    };
+
+    function cacheBookmarks(result) {
+      bookmarks = extract(result);
+      return bookmarks;
+    };
+
+    model.getBookmarks = function() {
+      return (bookmarks) ? $q.when(bookmarks) : $http.get(URLS.FETCH).then(cacheBookmarks);
+    }
+  })
+;
--- assets/js/common/models/categories-model.js
+++ assets/js/common/models/categories-model.js
@@ -1,1 +1,38 @@
-angular.module('eggly.models.categories', []);
+angular.module('eggly.models.categories', [])
+  .service("CategoriesModel", function($http, $q) {
+    var model = this,
+    URLS = {
+      FETCH: 'data/categories.json'
+    },
+    categories;
+
+    function extract(result) {
+      return result.data;
+    };
+
+    function cacheCategories(result) {
+      categories = extract(result);
+      return categories;
+    };
+
+    model.getCategories = function() {
+      return (categories) ? $q.when(categories) : $http.get(URLS.FETCH).then(cacheCategories);
+    };
+
+    model.getCategoryByName = function(categoryName) {
+      var deferred = $q.defer();
+
+      function findCategory() {
+        return _.find(categories, function(c){
+          return c.name == categoryName;
+        })
+      }
+
+      if( categories ) {
+        deferred.resolve(findCategory());
+      }
+      return deferred.promise;
+    };
+
+  })
+;
